507db67e728f5d4e21a48cad31832d81559e2f1a
Duke Vijitbenjaronk <warut.vijitbenjaronk@gmail.com>
2017-01-20 20:33:06 -0600

    Added file listener script

---
+++ hackerz/Assets/Scripts/CsvListener.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+public class CsvListener : MonoBehaviour {
+
+	public string DATA_PATH;
+
+	private string dataUrl;
+	private DateTime lastUpdate;
+
+	// Use this for initialization
+	void Start () {
+		dataUrl = Environment.CurrentDirectory+DATA_PATH;
+		//Add file handling behavior later
+		Debug.Log(System.IO.File.ReadAllText(dataUrl));
+		lastUpdate = System.IO.File.GetLastWriteTime(dataUrl);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Read file if updates have been written
+		if(System.IO.File.GetLastWriteTime(dataUrl) != lastUpdate){
+			//Add file handling behavior later
+			Debug.Log(System.IO.File.ReadAllText(dataUrl));
+			lastUpdate = System.IO.File.GetLastWriteTime(dataUrl);
+		}
+	}
+}
